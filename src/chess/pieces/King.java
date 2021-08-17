2a50860873d630a6db063c005ff2a3f921cd57d1
Iuri Almeida <ialmeida@vertigo.com.br>
2021-08-17 14:06:11 -0300

    Implementing possible moves of King

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -22,7 +23,54 @@ public class King extends ChessPiece {
 		
 		boolean[][] mat = new boolean[rows][columns];
 		
+		// auxiliary position
+		Position aux = new Position(0, 0);
+		
+		// piece position
+		int row = position.getRow();
+		int column = position.getColumn();
+		
+		// up
+		aux.setValues(row - 1, column);
+		if (getBoard().positionExists(aux) && canMove(aux)) mat[aux.getRow()][aux.getColumn()] = true;
+		
+		// right-up
+		aux.setValues(row - 1, column + 1);
+		if (getBoard().positionExists(aux) && canMove(aux)) mat[aux.getRow()][aux.getColumn()] = true;
+		
+		// right
+		aux.setValues(row, column + 1);
+		if (getBoard().positionExists(aux) && canMove(aux)) mat[aux.getRow()][aux.getColumn()] = true;
+		
+		// right-down
+		aux.setValues(row + 1, column + 1);
+		if (getBoard().positionExists(aux) && canMove(aux)) mat[aux.getRow()][aux.getColumn()] = true;
+		
+		// down
+		aux.setValues(row + 1, column);
+		if (getBoard().positionExists(aux) && canMove(aux)) mat[aux.getRow()][aux.getColumn()] = true;
+		
+		// left-down
+		aux.setValues(row + 1, column - 1);
+		if (getBoard().positionExists(aux) && canMove(aux)) mat[aux.getRow()][aux.getColumn()] = true;
+		
+		// left
+		aux.setValues(row, column - 1);
+		if (getBoard().positionExists(aux) && canMove(aux)) mat[aux.getRow()][aux.getColumn()] = true;
+		
+		// left-up
+		aux.setValues(row - 1, column - 1);
+		if (getBoard().positionExists(aux) && canMove(aux)) mat[aux.getRow()][aux.getColumn()] = true;
+		
 		return mat;
+	}
+	
+	private boolean canMove(Position position) {
+		
+		ChessPiece piece = (ChessPiece) getBoard().piece(position);
+		
+		return piece == null || piece.getColor() != getColor();
+		
 	}
 
 }
