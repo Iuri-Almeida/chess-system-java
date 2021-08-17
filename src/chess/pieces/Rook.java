14aa44117a8cdf9fddeb61672da1e990eef7e297
Iuri Almeida <ialmeida@vertigo.com.br>
2021-08-17 13:24:44 -0300

    Implementing possible moves of Rook

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -22,6 +23,57 @@ public class Rook extends ChessPiece {
 		int columns = getBoard().getRows();
 		
 		boolean[][] mat = new boolean[rows][columns];
+		
+		// auxiliary position
+		Position aux = new Position(0, 0);
+		
+		// piece position
+		int row = position.getRow();
+		int column = position.getColumn();
+		
+		// up
+		aux.setValues(row - 1, column);
+		while (getBoard().positionExists(aux) && !getBoard().thereIsAPiece(aux)) {
+			
+			mat[aux.getRow()][aux.getColumn()] = true;
+			aux.setRow(aux.getRow() - 1);
+			
+		}
+		
+		if (getBoard().positionExists(aux) && isThereOpponentPiece(aux)) mat[aux.getRow()][aux.getColumn()] = true;
+		
+		// right
+		aux.setValues(row, column + 1);
+		while (getBoard().positionExists(aux) && !getBoard().thereIsAPiece(aux)) {
+			
+			mat[aux.getRow()][aux.getColumn()] = true;
+			aux.setColumn(aux.getColumn() + 1);
+			
+		}
+		
+		if (getBoard().positionExists(aux) && isThereOpponentPiece(aux)) mat[aux.getRow()][aux.getColumn()] = true;
+		
+		// down
+		aux.setValues(row + 1, column);
+		while (getBoard().positionExists(aux) && !getBoard().thereIsAPiece(aux)) {
+			
+			mat[aux.getRow()][aux.getColumn()] = true;
+			aux.setRow(aux.getRow() + 1);
+			
+		}
+		
+		if (getBoard().positionExists(aux) && isThereOpponentPiece(aux)) mat[aux.getRow()][aux.getColumn()] = true;
+		
+		// left
+		aux.setValues(row, column - 1);
+		while (getBoard().positionExists(aux) && !getBoard().thereIsAPiece(aux)) {
+			
+			mat[aux.getRow()][aux.getColumn()] = true;
+			aux.setColumn(aux.getColumn() - 1);
+			
+		}
+		
+		if (getBoard().positionExists(aux) && isThereOpponentPiece(aux)) mat[aux.getRow()][aux.getColumn()] = true;
 		
 		return mat;
 	}
